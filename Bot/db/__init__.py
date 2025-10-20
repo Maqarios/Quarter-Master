4bf0973e21de597d0ba024ca7ed727367d54f1f6
maqarios <maqarioswagdy@gmail.com>
2025-10-20 18:16:19 +0000

    Refactor __init__.py to improve structure and clarity of database package

--- Bot/db/__init__.py
+++ Bot/db/__init__.py
@@ -1,2 +1,12 @@
 # Bot/db/__init__.py
 """Database package for the Quarter Master Bot."""
+
+from .database import Base, check_db_connection, db_engine, db_session, get_db_context
+
+__all__ = [
+    "Base",
+    "db_engine",
+    "db_session",
+    "get_db_context",
+    "check_db_connection",
+]
